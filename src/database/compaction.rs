0dd3f2af70bdd599bd22fb7fa09e4851cb84a8c5
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2019-08-19 15:51:54 +0300

    WIP

--- src/database/compaction.rs
+++ src/database/compaction.rs
@@ -1,25 +1,26 @@
 //! Compaction
-use super::Database;
 use super::key::Key;
+use super::Database;
 use leveldb_sys::leveldb_compact_range;
 use libc::{c_char, size_t};
 
-pub trait Compaction<'a, K: Key + 'a> {
+pub trait Compaction<'a, 'key: 'a, K: Key<'key>> {
     fn compact(&self, start: &'a K, limit: &'a K);
 }
 
-impl<'a, K: Key + 'a> Compaction<'a, K> for Database<K> {
+impl<'a, 'key: 'a, K: Key<'key>> Compaction<'a, 'key, K> for Database<'key, K> {
     fn compact(&self, start: &'a K, limit: &'a K) {
         unsafe {
-            start.as_slice(|s| {
-                limit.as_slice(|l| {
-                    leveldb_compact_range(self.database.ptr,
-                                          s.as_ptr() as *mut c_char,
-                                          s.len() as size_t,
-                                          l.as_ptr() as *mut c_char,
-                                          l.len() as size_t);
-                });
-            });
+            let s = start.as_ref();
+            let l = limit.as_ref();
+
+            leveldb_compact_range(
+                self.database.ptr,
+                s.as_ptr() as *mut c_char,
+                s.len() as size_t,
+                l.as_ptr() as *mut c_char,
+                l.len() as size_t,
+            );
         }
     }
 }
